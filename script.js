53ace020a52fff6b6ea23c4a47de374d882a808d
shota shavadze <shot.shavadze@gmail.com>
2023-07-19 14:24:41 +0400

    get all user and render

--- script.js
+++ script.js
@@ -5,3 +5,43 @@ const btnAll = document.getElementById('btn-all');
 const btnActive = document.getElementById('btn-active');
 const btnACompleted = document.getElementById('btn-completed');
 const toDoList = document.getElementById('to-do-list');
+
+fetch('https://dummyjson.com/users')
+    .then(res => res.json())
+    .then(allUser => {
+        const users = allUser.users
+        // console.log(users)
+
+        for (user of users) {
+            const singleUserDiv = document.createElement('div');
+            singleUserDiv.classList.add('single-user-div');
+            singleUserDiv.innerText = user.firstName + user.lastName;
+            employeesList.append(singleUserDiv);
+            let id = user.id;
+            singleUserDiv.addEventListener('click', function () {
+                getTodo(id)
+            });
+        }
+
+
+    });
+
+function getTodo(id) {
+
+    console.log(id)
+    fetch('https://dummyjson.com/todos')
+        .then(res => res.json())
+        .then(allTodo => {
+            console.log(allTodo.todos)
+
+
+
+
+
+            const singleUserTodo = document.createElement('div');
+            singleUserTodo.innerText = id;
+            toDoList.append(singleUserTodo)
+
+        })
+
+}
