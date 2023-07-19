aae38d81bb08b45725ff2b476d95e3bfaeeb6d5a
shota shavadze <shot.shavadze@gmail.com>
2023-07-19 12:56:19 +0400

    Prepear for Dom manipulation

---
+++ script.js
@@ -0,0 +1,7 @@
+const employeesList = document.getElementById('employees-list');
+const input = document.getElementById('input');
+const inputBtn = document.getElementById('input-btn');
+const btnAll = document.getElementById('btn-all');
+const btnActive = document.getElementById('btn-active');
+const btnACompleted = document.getElementById('btn-completed');
+const toDoList = document.getElementById('to-do-list');
