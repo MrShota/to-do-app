117721e6be49114100a51e2d25572f711a20d910
shota shavadze <shot.shavadze@gmail.com>
2023-07-20 21:35:00 +0400

    commit

--- script.js
+++ script.js
@@ -5,30 +5,35 @@ const btnAll = document.getElementById('btn-all');
 const btnActive = document.getElementById('btn-active');
 const btnACompleted = document.getElementById('btn-completed');
 const toDoList = document.getElementById('to-do-list');
-
+let singleUserDiv = '';
 fetch('https://dummyjson.com/users')
     .then(res => res.json())
     .then(allUser => {
         const users = allUser.users
         for (user of users) {
-            const singleUserDiv = document.createElement('div');
+            singleUserDiv = document.createElement('div');
+            singleUserDiv.setAttribute('id', 'singleUserDiv')
             singleUserDiv.classList.add('single-user-div');
             singleUserDiv.innerText = user.firstName + ' ' + user.lastName;
+            const userId = user.id;
+            // console.log(user)
             employeesList.append(singleUserDiv);
-            let userId = user.userId;
-            console.log(userId)
-            // singleUserDiv.addEventListener('click', function () {
-            //     fetch('https://dummyjson.com/todos')
-            //         .then(res => res.json())
-            //         .then(allTodo => {
-            //             const todo = allTodo.todos[userId];
-            //             const singleUserTodo = document.createElement('div')
-
-            //             singleUserTodo.append(todo.todo);
-            //             toDoList.append(singleUserTodo)
-            //             console.log(todo)
-
-            //         })
-            // });
+            singleUserDiv.addEventListener('click', function () {
+                // getAllTodo(userId)
+            })
         }
     });
+
+// function getAllTodo(id) {
+fetch('https://dummyjson.com/todos')
+    .then(res => res.json())
+    .then(allTodo => {
+        console.log(allTodo.todos)
+        for (userId in allTodo.todos) {
+            console.log()
+        }
+
+
+        // toDoList.innerText = allTodo.todos[id].todo
+    })
+// }
